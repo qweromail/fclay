4f8291dc949bbf7a4964adef11cbce8df2a8f8c8
Rustam Galiev <qweromail@gmail.com>
2016-11-04 16:39:04 +0300

    Added test generator part 3

--- lib/generators/initializer_generator.rb
+++ lib/generators/initializer_generator.rb
@@ -1,5 +1,9 @@
-class InitializerGenerator < Rails::Generators::Base
-  def create_initializer_file
-    create_file "config/initializers/initializer.rb", "# Add initialization content here"
+module Fclay
+  module Generators
+    class InitializerGenerator < Rails::Generators::Base
+      def create_initializer_file
+        create_file "config/initializers/initializer.rb", "# Add initialization content here"
+      end
+    end
   end
 end
