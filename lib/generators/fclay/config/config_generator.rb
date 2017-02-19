2a4bf0d303403ca09345c562908943c05ac4997e
Rustam Galiev <qweromail@gmail.com>
2017-02-19 18:16:48 +0300

    Fix

--- lib/generators/fclay/config/config_generator.rb
+++ lib/generators/fclay/config/config_generator.rb
@@ -2,10 +2,6 @@ require 'rails/generators/active_record'
 
 class Fclay::ConfigGenerator < ActiveRecord::Generators::Base
  
-  def self.source_root
-    @source_root ||= File.expand_path('../templates', __FILE__)
-  end
-
   def create_initializer_file
     create_file "config/initializers/fclay.rb" do 
 "Fclay.configure do |config|
