1c1e2717c69bf6f4ffe68ee374098a338b38cca7
Rustam Galiev <qweromail@gmail.com>
2017-02-22 22:17:35 +0300

    Bug fix ver to .15

--- lib/generators/fclay/config/config_generator.rb
+++ lib/generators/fclay/config/config_generator.rb
@@ -5,7 +5,7 @@ class Fclay::ConfigGenerator < Rails::Generators::Base
   def create_initializer_file
     create_file "config/initializers/fclay.rb" do 
 "Fclay.configure do |config|
-  #config.local_storage_assets_host = 'http://mysite.com'
+  #config.local_storage_host = 'http://mysite.com'
   #config.storage_policy = :s3
   #config.storages do |storages|
   #  storages[:s3] = {
